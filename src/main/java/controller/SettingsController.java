f32535cbcd0c7e3e905d6ce2649c2ec07f9d8a7f
Christian42 <cchimani@htl-steyr.ac.at>
2022-05-06 09:40:59 +0200

    a

--- src/main/java/controller/SettingsController.java
+++ src/main/java/controller/SettingsController.java
@@ -2,16 +2,32 @@ package controller;
 
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SettingsController {
 
-    public Button asd;
-    public Label id;
+    public void initialize(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        SettingsController con = loader.getController();
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                System.out.println(keyEvent.getCode());
 
-    public void chimani(ActionEvent event) {
-        id.setText("hallo");
-
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/view/SettingsView.java
+++ src/main/java/view/SettingsView.java
@@ -2,4 +2,5 @@ package view;
 
 public class SettingsView {
 
+
 }
