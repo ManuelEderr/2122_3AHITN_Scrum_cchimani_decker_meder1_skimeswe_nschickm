7da8d998320b1e4bccbbd4bf80bbdb5a1ac0ceb8
skimeswe <skimeswe@htl-steyr.ac.at>
2022-05-06 09:13:51 +0200

    Packages, Klassen

--- src/main/java/controller/BattleshipController.java
+++ src/main/java/controller/BattleshipController.java
@@ -15,23 +15,5 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class BattleshipController {
-    public TextField einagbwelchesfeld;
-    public Button sub;
-    @FXML
-    private Label welcomeText;
 
-
-    public void initialize(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-
-            }
-        });
-        stage.setScene(scene);
-        stage.show();
-    }
 }
