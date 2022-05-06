1550353ab6e2d05b52afd7879fb9d9743d4026d3
Christian42 <cchimani@htl-steyr.ac.at>
2022-05-06 09:08:47 +0200

    a

--- src/main/java/controller/BattleshipController.java
+++ src/main/java/controller/BattleshipController.java
@@ -1,14 +1,37 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class BattleshipController {
+    public TextField einagbwelchesfeld;
+    public Button sub;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void initialize(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
     }
 }
