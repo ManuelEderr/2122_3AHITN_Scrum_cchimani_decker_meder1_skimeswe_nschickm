f45a3116842d65ebb12c10ac6ce7364e8274dbc9
skimeswe <skimeswe@htl-steyr.ac.at>
2022-05-19 14:18:27 +0200

    Playfield

--- src/main/java/model/Playfield.java
+++ src/main/java/model/Playfield.java
@@ -36,19 +36,63 @@ public class Playfield {
 
     /**
      * Diese Methode prüft, ob sich auf der gewählten Position ein Schiff befindet.
+     *
      * @param ship
      */
-    public void checkShip(Ship ship) {
-       // Iterator<Coordinate> iterator = ship.coords.iterator();
-      //  while (iterator.hasNext()) {
-         //   Coordinate field = iterator.next();
-          //  if ()
-        }
-    //}
+    public boolean checkShip(Ship ship) {
+        boolean rv = true;
+        Iterator<Coordinate> iterator = ship.coord.iterator();
+        while (iterator.hasNext()) {
+            Coordinate field = iterator.next();
+            if (field.x >= MAX_X || field.y >= MAX_Y) {
+                rv = false;
 
-    public void placeShip(Ship ship) {
+            }
+            if (feld[field.x][field.y] != 0) {
+                rv = false;
+            }
+
+        }
+        return rv;
+
 
     }
 
+    public void placeShip(Ship ship) {
+        Iterator<Coordinate> iterator = ship.coord.iterator();
+        while (iterator.hasNext()) {
+            Coordinate place = iterator.next();
+            if (feld[place.x][place.y] != 0) {
+                System.out.println("Feld bereits belegt");
+            } else {
+                feld[place.x][place.y] = 2;
+            }
+        }
+        flotte.add(ship);
+    }
+
+    public boolean checkHit(Coordinate coordinate) {
+        boolean rv = false;
+        int position = feld[coordinate.x][coordinate.y];
+        if (position == 0 || position == 2) {
+            rv = true;
+        }
+        return rv;
+
+    }
+
+    public int placeHit(Coordinate coordinate) {
+        if (feld[coordinate.x][coordinate.y] == 1 || feld[coordinate.x][coordinate.y] == 3) {
+            System.out.println("bereits getroffen");
+        } else if (feld[coordinate.x][coordinate.y] == 0) {
+            System.out.println("Wasser");
+
+        } else if (feld[coordinate.x][coordinate.y] == 2) {
+
+        }
+
+        return 1;
+    }
 
 }
+
