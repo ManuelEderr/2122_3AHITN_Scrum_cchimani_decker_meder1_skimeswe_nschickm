227f13609173448f4a078b6001c99dec34f70475
Christian42 <cchimani@htl-steyr.ac.at>
2022-05-19 13:55:16 +0200

    a

--- src/main/java/model/Coordinate.java
+++ src/main/java/model/Coordinate.java
@@ -1,4 +1,26 @@
 package model;
 
+import java.util.Objects;
+
 public class Coordinate {
+    public Integer x;
+    public Integer y;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return Float.compare(that.x, x) == 0 && Float.compare(that.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public Coordinate(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
 }
