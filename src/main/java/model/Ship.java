3f8501c5d0c97139dd747a6400d76d512eac642a
nschickm <nschickm@htl-steyr.ac.at>
2022-05-19 14:21:37 +0200

    a

--- src/main/java/model/Ship.java
+++ src/main/java/model/Ship.java
@@ -9,15 +9,13 @@ import java.util.ArrayList;
  * 2 Kreuzer (je 4 Kästchen)
  * 3 Zerstörer (je 3 Kästchen)
  * 4 U-Boote (je 2 Kästchen)
- *
+ * <p>
  * Die Ship Klasse speichert Start- und Endwert
- *
+ * <p>
  * Wittner:
  * Schiff ist Laenge, Position und Ausrichtung
- *
  */
 public class Ship {
-
 
 
     private static Coordinate coord1 = null;
@@ -27,63 +25,73 @@ public class Ship {
     private static Coordinate coord5 = null;
     private String shipname = "";
 
-    ArrayList<Coordinate> coord = new ArrayList<>();
+    ArrayList<Coordinate> coords = new ArrayList<>();
 
     //Schiff mit der größe 2
-    public void ship(Coordinate coord1, Coordinate coord2, String name){
+    public void ship(Coordinate coord1, Coordinate coord2, String name) {
         this.coord1 = coord1;
         this.coord2 = coord2;
-        coord.add(coord1);
-        coord.add(coord2);
+        coords.add(coord1);
+        coords.add(coord2);
         shipname = name;
     }
 
     //Schiff mit der größe 3
-    public void ship(Coordinate coord1, Coordinate coord2, Coordinate coord3, String name){
+    public void ship(Coordinate coord1, Coordinate coord2, Coordinate coord3, String name) {
         this.coord1 = coord1;
         this.coord2 = coord2;
         this.coord3 = coord3;
-        coord.add(coord1);
-        coord.add(coord2);
-        coord.add(coord3);
+        coords.add(coord1);
+        coords.add(coord2);
+        coords.add(coord3);
         shipname = name;
     }
 
     //Schiff mit der größe 4
-    public void ship(Coordinate coord1, Coordinate coord2, Coordinate coord3, Coordinate coord4, String name){
+    public void ship(Coordinate coord1, Coordinate coord2, Coordinate coord3, Coordinate coord4, String name) {
         this.coord1 = coord1;
         this.coord2 = coord2;
         this.coord3 = coord3;
         this.coord4 = coord4;
-        coord.add(coord1);
-        coord.add(coord2);
-        coord.add(coord3);
-        coord.add(coord4);
+        coords.add(coord1);
+        coords.add(coord2);
+        coords.add(coord3);
+        coords.add(coord4);
         shipname = name;
     }
 
     //Schiff mit der größe 5
-    public void ship(Coordinate coord1, Coordinate coord2, Coordinate coord3, Coordinate coord4, Coordinate coord5, String name){
+    public void ship(Coordinate coord1, Coordinate coord2, Coordinate coord3, Coordinate coord4, Coordinate coord5, String name) {
         this.coord1 = coord1;
         this.coord2 = coord2;
         this.coord3 = coord3;
         this.coord4 = coord4;
         this.coord5 = coord5;
-        coord.add(coord1);
-        coord.add(coord2);
-        coord.add(coord3);
-        coord.add(coord4);
-        coord.add(coord5);
+        coords.add(coord1);
+        coords.add(coord2);
+        coords.add(coord3);
+        coords.add(coord4);
+        coords.add(coord5);
         shipname = name;
     }
 
 
-    public void shipHit(Coordinate coord){
+    //Ueberprueft ob ein Schiff getroffen wird
+    public void shipHit(Coordinate coord) {
+        for (int i = 0; i < coords.size(); i++) {
 
+        }
     }
 
+    //Ueberprueft ob von einem getroffenen Schiff noch eine Koordinate nicht getroffen wurde
+    // also ob noch etwas von einem getroffenen Schiff uebrig ist
+    public boolean shipLeft() {
+        return coords.isEmpty();
+    }
 
+    // gibt den Namen des Schiffes zurueck
     public String getShipname() {
+        //  System.out.println(shipname);
         return shipname;
     }
 }
