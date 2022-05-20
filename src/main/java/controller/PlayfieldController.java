1caa2c0d5d548987ec8d60cbb5c0a2c6a753e28b
skimeswe <skimeswe@htl-steyr.ac.at>
2022-05-20 08:37:33 +0200

    Playfield

--- src/main/java/controller/PlayfieldController.java
+++ src/main/java/controller/PlayfieldController.java
@@ -1,18 +1,27 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PlayfieldController {
-    @FXML
-    private Button test1;
+    public GridPane boardView;
 
-    @FXML
-    private Label test;
+    public void initialize(Stage stage) throws IOException {
 
-    public void clcktest(ActionEvent event) {
-        test.setText("Weee");
     }
+
+
 }
