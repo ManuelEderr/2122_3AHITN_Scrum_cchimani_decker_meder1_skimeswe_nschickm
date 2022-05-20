c666443033550444659511d3d6edb2515caf5b44
decker213 <decker@htl-steyr.ac.at>
2022-05-20 08:15:26 +0200

    Settings

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,5 +1,7 @@
 package model;
 
+import javafx.scene.image.Image;
+
 /**
  * @author david
  * Speichert den Namen und den Punktestand eines Spielers.
@@ -7,13 +9,18 @@ package model;
 public class Player {
     String name;
     int score = 0;
+    Image background;
+    Image ship;
 
     /**
      * Im Konstruktor wird er Name des Spielers angelegt.
+     *
      * @param name -> Name des Spielers
      */
-    public Player(String name) {
+    public Player(String name, Image background, Image ship) {
         this.name = name;
+        this.background = background;
+        this.ship = ship;
     }
 
     /**
@@ -32,6 +39,7 @@ public class Player {
 
     /**
      * Addiert die übergebene Variable zum Punktestand dazu.
+     *
      * @param x -> jener Wert, welcher zum Punktestand addiert wird
      */
     public void addScore(int x) {
